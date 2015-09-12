38f04813260f26f945c4b252f3ebb2d66e20897c
DataScience <datascience@ragnar.localNetwork>
2015-09-12 15:46:02 -0400

    First pass for plot3

---
+++ plot3.R
@@ -0,0 +1,47 @@
+## Load the necessary libraries
+## Retrieve the data
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              destfile = "/tmp/household-data-set.zip",
+              method = "wget",
+              quiet = TRUE
+              )
+unzip("/tmp/household-data-set.zip", exdir = "/tmp")
+## Read in the data
+## NB: factor chosen to speed initial read
+householdData <- read.table(
+      "/tmp/household_power_consumption.txt", 
+      header = TRUE,
+      sep = ";", 
+      colClasses = c("factor",
+                     "factor",
+                     "factor",
+                     "factor",
+                     "factor",
+                     "factor",
+                     "factor",
+                     "factor",
+                     "factor"),
+      nrows = 2075260
+      )
+## Coerce the data into its proper data types
+## Create a synthetic date-time column
+householdData$FullDate <- paste(householdData$Date, householdData$Time)
+householdData$FullDate <- dmy_hms(householdData$FullDate)
+## Coerce factor in appropriate data types where necessary
+householdData$Date <- as.Date(dmy(householdData$Date))
+householdData$Sub_metering_1 <- as.numeric(
+      as.character(
+            householdData$Sub_metering_1
+      )
+)
+householdData$Sub_metering_2 <- as.numeric(
+      as.character(
+            householdData$Sub_metering_2
+      )
+)
+
+householdData$Sub_metering_3 <- as.numeric(
+      as.character(
+            householdData$Sub_metering_3
+      )
+)
